a5c81b1f979a35d8531b0f494f388620f996af0d
Daniel Levin <daniellevin2607@gmail.com>
2025-06-28 12:48:31 +0200

    take dep on tokio

---
+++ bakkie/tests/rag.rs
@@ -0,0 +1,5 @@
+/// Returns an exact count of the number of occurrences of the needle in the haystack.
+#[bakkie::tool]
+fn count_letters(haystack: String, needle: char) -> bakkie::Result<String> {
+    todo!();
+}
