7543a4f12169baca4c7ce8c974a2aa28446526ce
Daniel Levin <daniellevin2607@gmail.com>
2025-07-25 12:49:26 +0200

    begin new example

---
+++ util/analyst/src/main.rs
@@ -0,0 +1,113 @@
+use std::collections::HashMap;
+
+use bakkie::{
+    proto::V20250618::McpServer,
+    provisions::{
+        Provisions,
+        tools::{Result, ToolError},
+    },
+};
+
+use linkme::distributed_slice;
+
+#[distributed_slice]
+pub static SOURCES: [Source];
+
+struct Source {
+    name: &'static str,
+    feed: &'static str,
+}
+
+#[distributed_slice(SOURCES)]
+static NYT: Source = Source {
+    name: "New York Times",
+    feed: "https://rss.nytimes.com/services/xml/rss/nyt/World.xml",
+};
+
+#[distributed_slice(SOURCES)]
+static SKY_NEWS: Source = Source {
+    name: "Sky News",
+    feed: "https://feeds.skynews.com/feeds/rss/home.xml",
+};
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Rss {
+    pub channel: Channel,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Channel {
+    pub item: Vec<Story>,
+}
+
+#[bakkie::structured]
+pub struct Story {
+    pub title: String,
+}
+
+pub fn parse_rss(xml_content: &str) -> anyhow::Result<Vec<Story>> {
+    let rss: Rss = quick_xml::de::from_str(xml_content)?;
+    Ok(rss.channel.item)
+}
+
+async fn yoink_stories(link: &str) -> anyhow::Result<Vec<Story>> {
+    let body = reqwest::get(link).await?.text().await?;
+
+    tracing::info!("got stories {body}");
+
+    match parse_rss(&body) {
+        Ok(x) => Ok(x),
+        Err(e) => {
+            tracing::error!("poes! {e:#?}");
+            Err(e)
+        }
+    }
+}
+
+/// List news sources to which we have legitimate programmatic access.
+#[bakkie::tool]
+async fn list_news_sources() -> Result<Vec<String>> {
+    let mut sources = vec![];
+
+    for Source { name, .. } in SOURCES {
+        sources.push(name.to_string());
+    }
+
+    Ok(sources)
+}
+
+#[bakkie::tool]
+async fn yoink_top_stories(publication: String) -> Result<Vec<Story>> {
+    for Source { name, feed } in SOURCES {
+        if *name == publication {
+            return Ok(yoink_stories(feed).await.unwrap());
+        }
+    }
+
+    Ok(vec![])
+}
+
+#[test]
+fn poes() {
+    let x = include_str!("kak.xml");
+    let y = parse_rss(x);
+
+    dbg!(y);
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    bakkie::dnp!();
+
+    let provisions = Provisions::default();
+
+    //provisions.insert_tool(greet()).await;
+    provisions.insert_tool(yoink_top_stories).await;
+    provisions.insert_tool(list_news_sources).await;
+
+    let server = McpServer::new_with_provisions(bakkie::stdio(), provisions);
+    server.run().await?;
+    Ok(())
+}
--- util/digest/src/main.rs
+++ util/digest/src/main.rs
@@ -8,6 +8,58 @@ use bakkie::{
     },
 };
 
+use ashpd::desktop::Color;
+
+use std::{thread, time};
+
+use ashpd::desktop::{
+    Icon,
+    notification::{Action, Button, Notification, NotificationProxy, Priority},
+};
+use futures_util::StreamExt;
+use zbus::zvariant::Value;
+
+async fn run() -> ashpd::Result<()> {
+    let proxy = NotificationProxy::new().await?;
+
+    let notification_id = "org.gnome.design.Contrast";
+    proxy
+        .add_notification(
+            notification_id,
+            Notification::new("Contrast")
+                .default_action("open")
+                .default_action_target(100)
+                .body("color copied to clipboard")
+                .priority(Priority::High)
+                .icon(Icon::with_names(&["dialog-question-symbolic"]))
+                .button(Button::new("Copy", "copy").target(32))
+                .button(Button::new("Delete", "delete").target(40)),
+        )
+        .await?;
+
+    /*
+    let action = proxy
+        .receive_action_invoked()
+        .await?
+        .next()
+        .await
+        .expect("Stream exhausted");
+    match action.name() {
+        "copy" => (),   // Copy something to clipboard
+        "delete" => (), // Delete the file
+        _ => (),
+    };
+    println!("{:#?}", action.id());
+    println!(
+        "{:#?}",
+        action.parameter().get(0).unwrap().downcast_ref::<u32>()
+    );
+
+    proxy.remove_notification(notification_id).await?;
+    */
+    Ok(())
+}
+
 /// Tabulates the characters that appear in the input for accurate counting.
 #[bakkie::tool(title = "__count_letters")]
 async fn count_letters(input: String) -> Result<HashMap<char, usize>> {
