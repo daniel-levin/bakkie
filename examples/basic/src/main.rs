f77805da8005850e9596eec64ddb295a96580d00
Daniel Levin <daniellevin2607@gmail.com>
2025-08-04 17:00:53 +0200

    basic example

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -1,52 +1,24 @@
 use bakkie::{
-    App, McpServer, Provisions,
+    McpServer, Provisions,
     provisions::tools::{Result, ToolError},
 };
-use std::collections::HashMap;
+use sha2::{Digest, Sha256};
 
-#[derive(Debug, Default)]
-pub struct MyApp {
-    invocations: usize,
-}
-
-/// Tabulates the characters that appear in the input for accurate counting.
-#[bakkie::tool(title = "__count_letters")]
-async fn count_letters(input: String) -> Result<HashMap<char, usize>> {
-    let mut res = HashMap::new();
-    for ch in input.chars() {
-        *res.entry(ch).or_insert(0) += 1;
-    }
-    Ok(res)
-}
-
-#[bakkie::structured]
-#[derive(PartialEq, Eq)]
-enum Progress {
-    Forward,
-    Backward,
-}
-
-/// Inform the user of some progress
-#[bakkie::tool(title = "__inform_progress")]
-async fn inform_progress(#[app] app: App<MyApp>, progress: Progress) -> Result<usize> {
-    let mut c = app.write().await;
-    if progress == Progress::Forward {
-        c.invocations += 1;
-    }
-    Ok(c.invocations)
+/// Compute the sha256 of a string and return it in hex representation
+#[bakkie::tool(title = "__basic__compute_sha2")]
+async fn compute_sha256(input: String) -> Result<String> {
+    let mut hasher = Sha256::new();
+    hasher.update(input.as_bytes());
+    let result = hasher.finalize();
+    Ok(format!("{:x}", result))
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let provisions = Provisions::default();
-    provisions.insert_tool(count_letters).await;
-    provisions.insert_tool(inform_progress).await;
+    provisions.insert_tool(compute_sha256).await;
 
-    let server = McpServer::new_with_provisions_and_application(
-        bakkie::stdio(),
-        provisions,
-        MyApp::default(),
-    );
+    let server = McpServer::new_with_provisions(bakkie::stdio(), provisions);
     server.run().await?;
     Ok(())
 }
